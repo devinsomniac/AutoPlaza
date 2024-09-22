dbfe09aa14ede41f122ad3b51c0ff32116acc4c9
Inzamam <inzamamchowdhury20@gmail.com>
2024-09-22 23:10:17 +0600

    Front end - Car List Form

--- src/add-listing/AddListing.jsx
+++ src/add-listing/AddListing.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import Header from "@/components/Header";
+import React from "react";
+import carDetails from "./../Shared/carDetails.json";
+import InputField from "./components/InputField";
+import DropDown from "./components/DropDown";
+import TextArea from "./components/TextArea";
 
 const AddListing = () => {
   return (
-    <div>AddListing</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="px-10 md:px-20 my-10">
+        <h2 className="font-bold text-4xl">Add New Listing</h2>
+        <form className="p-10 border rounded-xl mt-10">
+          {/* car Details */}
+          <div>
+            <h2 className="font-medium text-xl mb-6">Car Details</h2>
+            <div  className="grid grid-cols-1 md:grid-cols-2 gap-5">
+              {carDetails.carDetails.map((item, index) => (
+                <div>
+                  <label className="text-sm " >{item.label} {item.required&& <span className="text-red-700">*</span>}</label>
+                  {item.fieldType == "text" || item.fieldType == "number" ? (
+                    <InputField item={item} />
+                  ) : item.fieldType == "dropdown" ? <DropDown item={item} />
+                  : item.fieldType == "textarea" ? <TextArea item={item}/>
+                  :null}
+                </div>
+              ))}
+            </div>
+          </div>
 
-export default AddListing
+          {/* feature List */}
+          {/* car Image */}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddListing;
---
+++ src/add-listing/components/TextArea.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Textarea } from "@/components/ui/textarea"
+
+const TextArea = () => {
+  return (
+    <div>
+        <Textarea />
+    </div>
+  )
+}
+
+export default TextArea
