07584730f6741bf69dacfb5e86e53c102fa03187
Inzamam <inzamamchowdhury20@gmail.com>
2024-10-03 19:04:25 +0600

    The Most Searched component is linked with database instead of fake data

--- src/components/MostSearchCar.jsx
+++ src/components/MostSearchCar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState ,useEffect} from "react";
 import fakeData from "@/Shared/fakeData";
 import CarItem from "./CarItem";
 import {
@@ -8,9 +8,26 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import {db} from './../../configs'
+import { CarImages, CarList } from './../../configs/schema'
+import { desc, eq } from 'drizzle-orm'
+import {FormatResult} from './../Shared/Service';
 
 const MostSearchCar = () => {
-  console.log(fakeData.carList);
+  const [carList,setCarList] = useState([])
+  useEffect(() => {
+    getPopularCarList();
+  }, []);
+  const getPopularCarList = async() => {
+    const result = await db.select()
+    .from(CarList)
+    .leftJoin(CarImages,eq(CarList.id,CarImages.carListId))
+    .orderBy(desc(CarList.id))
+    .limit(6)
+    const res = FormatResult(result)
+    setCarList(res)
+    console.log(res)
+  }
   return (
     <div className="mx-24 hidden md:block">
       <h2 className="font-bold text-3xl text-center my-16">
@@ -18,7 +35,7 @@ const MostSearchCar = () => {
       </h2>
       <Carousel>
         <CarouselContent>
-          {fakeData.carList.map((car, index) => (
+          {carList.map((car, index) => (
             <CarouselItem className="basis-1/4">
               <CarItem car={car} />
             </CarouselItem>
