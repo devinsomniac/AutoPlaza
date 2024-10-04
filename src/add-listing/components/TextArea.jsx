0b83386129afb0001a51fed48dad066f0dd6a526
Inzamam <inzamamchowdhury20@gmail.com>
2024-10-04 22:19:56 +0600

    Added functionality to edit text area of the listing

--- src/add-listing/components/TextArea.jsx
+++ src/add-listing/components/TextArea.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Textarea } from "@/components/ui/textarea"
 
-const TextArea = ({item,handleInputChange}) => {
+const TextArea = ({item,handleInputChange,carInfo}) => {
   return (
     <div>
-        <Textarea name={item.name} onChange = {(e)=>handleInputChange(item.name,e.target.value)} />
+        <Textarea name={item.name}
+        defaultValue = {carInfo ? carInfo[item.name] : ""}  
+        onChange = {(e)=>handleInputChange(item.name,e.target.value)} />
     </div>
   )
 }
