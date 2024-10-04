54d259df16196c4fce7f0d285322072d2f606e44
Inzamam <inzamamchowdhury20@gmail.com>
2024-10-04 22:19:33 +0600

    Added functionality to edit inputfield of the listing

--- src/add-listing/components/InputField.jsx
+++ src/add-listing/components/InputField.jsx
@@ -1,10 +1,14 @@
 import { Input } from '@/components/ui/input'
 import React from 'react'
 
-const InputField = ({item,handleInputChange}) => {
+const InputField = ({item,handleInputChange,carInfo}) => {
   return (
     <div>
-        <Input type={item?.fieldType} name={item?.name} required={item?.required} onChange = {(e)=>handleInputChange(item.name,e.target.value)}/>
+        <Input type={item?.fieldType} 
+        name={item?.name} 
+        required={item?.required}
+        defaultValue = {carInfo ? carInfo[item.name] : ""} 
+        onChange = {(e)=>handleInputChange(item.name,e.target.value)}/>
     </div>
   )
 }
