105c8da2020890b5140e1ca04a0ccc4b2c45eb38
Inzamam <inzamamchowdhury20@gmail.com>
2024-10-04 22:18:08 +0600

    Added functionality to edit details

--- src/add-listing/components/CheckBox.jsx
+++ src/add-listing/components/CheckBox.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Checkbox } from "@/components/ui/checkbox"
 
-const CheckBox = ({item,handleFeatureChange}) => {
+const CheckBox = ({item,handleFeatureChange,feature}) => {
   return (
     <div>
-        <Checkbox name={item.label} onCheckedChange = {(value) => handleFeatureChange(item.label,value)} />
+        <Checkbox name={item.label} checked={feature?.[item.label] } onCheckedChange = {(value) => handleFeatureChange(item.label,value)} />
     </div>
   )
 }
