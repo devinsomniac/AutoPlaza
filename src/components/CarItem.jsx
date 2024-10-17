49a9a1c6f4187bd88d2610a0661083321758cd10
Inzamam <inzamamchowdhury20@gmail.com>
2024-10-17 16:12:02 +0600

    Added responsive func

--- src/components/CarItem.jsx
+++ src/components/CarItem.jsx
@@ -10,7 +10,7 @@ const CarItem = ({car}) => {
   const imageUrl = car?.images?.[0]?.imageUrl || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSekpeEIJGU-elYdLvk4w0Ih04PQ-lg6ce4DfRcU-Jl3Bw3VQzhOdAbCZEjL1QpHZp383A&usqp=CAU'; 
   return (
     <Link to={'/listing-details/'+car?.id}>
-    <div className="rounded-xl bg-white border hover:shadow-2xl cursor-pointer object-cover">
+    <div className="rounded-xl w-full bg-white border hover:shadow-2xl cursor-pointer object-cover">
         <img className='rounded-t-xl h-[180px]' src={imageUrl} width={'100%'} height={250}/>
         <div className="p-4">
             <h2 className=' text-black text-xs md:text-lg md:font-bold mb-2'>{car?.listingTitle}</h2>
